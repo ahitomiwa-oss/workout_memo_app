390839dc84da8ed8525972bac48f5d92e7ea4d47
ahitomiwa-oss <ahito.miwa@gmail.com>
2026-06-10 20:50:12 +0900

    tableの作成,取得,保存を追加

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -30,4 +30,43 @@ class DatabaseHelper {
     );
   }
 
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE workout_records (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        exercise TEXT NOT NULL,
+        weight REAL NOT NULL,
+        reps INTEGER NOT NULL,
+        sets INTEGER NOT NULL,
+        date TEXT NOT NULL
+      )
+    ''');
+  }
+
+  //記録の保存
+  Future<WorkoutRecord> create(WorkoutRecord record) async {
+    final db = await instance.database;
+    final id = db.insert('workout_records', record.toMap());
+    return WorkoutRecord(
+      id: id,
+      exercise: record.exercise,
+      weight: record.weight, 
+      reps: record.reps, 
+      sets: record.sets, 
+      date: record.date,
+      );
+  }
+
+  //全記録の取得
+  Future<List<WorkoutRecord>> readAllRecords() async {
+    final db = await instance.database;
+    final result = await db.query('workout_records', orderBy: 'date DESC');
+    return result.map((map) => WorkoutRecord.fromMap(map)).toList();
+  }
+
+  //データベースを閉じる
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
 }
