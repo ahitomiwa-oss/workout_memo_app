11e01e5f4449a0671ef27f2578c3ba835e38dafc
ahitomiwa-oss <ahito.miwa@gmail.com>
2026-06-11 22:33:51 +0900

    コメントの追加

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -33,7 +33,7 @@ class DatabaseHelper {
   Future _createDB(Database db, int version) async {
     await db.execute('''
       CREATE TABLE workout_records (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
         exercise TEXT NOT NULL,
         weight REAL NOT NULL,
         reps INTEGER NOT NULL,
@@ -41,12 +41,13 @@ class DatabaseHelper {
         date TEXT NOT NULL
       )
     ''');
+    //AUTOINCREMENTでデータを保存するたびに1,2,3と増える
   }
 
   //記録の保存
   Future<WorkoutRecord> create(WorkoutRecord record) async {
     final db = await instance.database;
-    final id = db.insert('workout_records', record.toMap());
+    final id = await db.insert('workout_records', record.toMap());
     return WorkoutRecord(
       id: id,
       exercise: record.exercise,
@@ -60,7 +61,7 @@ class DatabaseHelper {
   //全記録の取得
   Future<List<WorkoutRecord>> readAllRecords() async {
     final db = await instance.database;
-    final result = await db.query('workout_records', orderBy: 'date DESC');
+    final result = await db.query('workout_records', orderBy: 'date DESC'); // date（日付）の順番でデータを並べる DESC = 降順（新しい順）
     return result.map((map) => WorkoutRecord.fromMap(map)).toList();
   }
 
